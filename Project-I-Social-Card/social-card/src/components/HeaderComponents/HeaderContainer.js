6c86b772510db991942fff713bb6862da893e76a
cahaug <caleahaug@gmail.com>
2019-05-29 17:17:12 -0700

    working, still hasn't compiled successfully so continuing to build out content

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from '../HeaderComponents/HeaderContent'
+import HeaderTitle from './components/HeaderComponents/HeaderTitle';
+import ImgThumbnail from '../HeaderComponents/ImageThumbnail'
+
+const HeaderContainer = () => {
+    return (<div>
+        <ImgThumbnail />
+        <div>
+            <HeaderTitle />
+            <HeaderContent />
+        </div>
+    </div>)
+}
+
+export default HeaderContainer;
