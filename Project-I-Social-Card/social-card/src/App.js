2c846e4f5a6d89317024f5ceb9a9022b84b7b722
cahaug <caleahaug@gmail.com>
2019-05-29 17:17:49 -0700

    left one file unsaved

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
+import CardBanner from './components/CardComponents/CardBanner';
 import CardContainer from './components/CardComponents/CardContainer';
+import CardContent from './components/CardComponents/CardContent';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
-import HeaderContent from './components/HeaderComponents/HeaderContent';
-import HeaderTitle from './components/HeaderComponents/HeaderTitle';
-import ImageThumbnail from './components/HeaderComponents/ImageThumbnail';
+
 import Footer from './components/FooterComponents/Footer';
 
 const App = () => {
   return (
     <div className="App">
       
-        <Header />
+        <HeaderContainer />
         <CardContainer />
   
     </div>
